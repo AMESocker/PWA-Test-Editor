4f95aed4b358006bc0d658071b2e1554acb2a722
AMESocker <aesocher@yahoo.com>
2022-12-01 20:21:36 -0800

    fixed error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,8 +15,8 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jateDb', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const db = await openDB('jateDb', 1);
+  const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ jate: content });
   const result = await request;
@@ -26,8 +26,8 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
+  const db = await openDB('jateDb', 1);
+  const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
